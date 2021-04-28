abc7d3c9281690698f4db268b069922de0ed6236
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-28 23:24:29 +0900

    solve question

---
+++ A-Move-and-Win.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, A, B;
+  cin >> N >> A >> B;
+
+  if ((B - A) % 2 == 0) cout << "Alice" << endl;
+  else cout << "Borys" << endl;
+  
+  return 0;
+}
